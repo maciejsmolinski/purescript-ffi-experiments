0c6b1f8b4bae62e59083ff84e29697de4645ded9
Maciej Smolinski <contact@maciejsmolinski.com>
2018-05-25 01:49:08 +0200

    PureScript: Define a helper funuction `greetUser`

--- src/Main.purs
+++ src/Main.purs
@@ -6,9 +6,12 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Interop (User(..), greet, message, user)
 
+greetUser :: User -> String
+greetUser = greet <<< show
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log message
   log $ greet "from Purescript"
-  log $ greet $ show user
-  log $ greet $ show $ MkUser { name: "PureScriptOne" }
+  log $ greetUser user
+  log $ greetUser $ MkUser { name: "PureScriptOne" }
