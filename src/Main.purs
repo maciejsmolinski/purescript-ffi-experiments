18504cc246f05ec245db4fd28e156d61b2e261ff
Maciej Smolinski <contact@maciejsmolinski.com>
2018-05-29 18:34:16 +0200

    Examples: Change the text of the notifications being rendered.

--- src/Main.purs
+++ src/Main.purs
@@ -16,9 +16,9 @@ onFailure value = log $ "Failed with " <> value
 
 main :: forall e. Eff (html :: I.HTML, dom :: PS.DOM, console :: CONSOLE | e) Unit
 main = do
-  I.render "Message rendered using PureScript FFI"
-  PS.render "Message rendered using PureScript-DOM"
-  I.append "Message appended using PureScript FFI"
-  PS.append "Message appended using PureScript-DOM"
+  I.render "Message rendered with PureScript FFI"
+  PS.render "Message rendered with PureScript-DOM"
+  I.append "Message appended with PureScript FFI"
+  PS.append "Message appended with PureScript-DOM"
   I.succeed onSuccess onFailure
   I.fail onSuccess onFailure
